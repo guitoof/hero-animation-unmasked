5426287eb05216d2018ba3e500de357a4e94ae1f
Guillaume Diallo-Mulliez <guillaumedm@bam.tech>
2021-05-29 10:10:01 +0200

    :construction: (UnmaskedController) Check flight validity & ignore if does
    not have a valid origin & destination

--- lib/packages/unmasked_hero/unmasked_hero_controller.dart
+++ lib/packages/unmasked_hero/unmasked_hero_controller.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/widgets.dart';
 
 class UnmaskedHeroController extends NavigatorObserver {
+  /// Checks whether the hero's flight has a valid origin & destination routes
+  bool _isFlightValid(PageRoute? fromRoute, PageRoute toRoute) {
+    if (fromRoute == null) {
+      return false;
+    }
+    BuildContext? fromRouteContext = fromRoute.subtreeContext;
+    BuildContext? toRouteContext = toRoute.subtreeContext;
+    if (fromRouteContext == null || toRouteContext == null) {
+      return false;
+    }
+    return true;
+  }
+
   @override
-  void didPush(Route<dynamic> route, Route<dynamic>? previousRoute) {
-    print('Navigating from $previousRoute to $route');
-    super.didPush(route, previousRoute);
+  void didPush(Route<dynamic> toRoute, Route<dynamic>? fromRoute) {
+    WidgetsBinding.instance?.addPostFrameCallback((Duration value) {
+      /// If the flight is not valid, let's just ignore the case
+      if (!_isFlightValid(fromRoute as PageRoute?, toRoute as PageRoute)) {
+        return;
+      }
+    });
+    super.didPush(toRoute, fromRoute);
   }
 }
